4ec9e02dcd4b364b0078502ccf2068e62f350e93
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-03-19 21:27:59 +1100

    Update Code

--- src/main/java/com/youtube/indianmovies/data/GeolocationSearch.java
+++ src/main/java/com/youtube/indianmovies/data/GeolocationSearch.java
@@ -30,7 +30,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 /**
  * This sample lists videos that are associated with a particular keyword and are in the radius of
